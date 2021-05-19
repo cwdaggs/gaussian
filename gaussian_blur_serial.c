6b5372154b905273f3cdd01598298185124af7fa
Cameron Daggs <cwdaggs@ucdavis.edu>
2021-05-18 19:43:53 -0700

    Bunch of gaussian calc

--- gaussian_blur_serial.c
+++ gaussian_blur_serial.c
@@ -4,8 +4,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define M_PI acos(-1.0)
 
-void write_mandelmap(char *filename, unsigned char *mandelmap, size_t N, int cutoff)
+void gaussian_calc(unsigned char *image_mat, float *kernel, int width, int height, float order) 
+{
+    int val = 0;
+
+    int center = ((int) order - 1) / 2;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            for (int x = 0; x < (int) order; x++) {
+                for (int y = 0; y < (int) order; y++) {
+                    if (i < center || j < center) {
+                        if (x <= center && y <= center) {
+                            val += (image_mat[i * (int) order + j] * kernel[x * (int) order + y]);
+                        } else if (x < center && y > center) {
+                            val += (image_mat[i * (int) order + j + (y - center)] * kernel[x * (int) order + y]);
+                        } else if (y < center && x > center) {
+                            val += (image_mat[(i + (x - center)) * (int) order + j] * kernel[x * (int) order + y]);
+                        }
+                    }
+                }
+            }
+            image_mat[i * (int) order + j] = val; 
+            val = 0;
+        }
+    }
+}
+
+void write_mandelmap(char *filename, unsigned char *mandelmap, int width, int height)
 {
 	FILE *fp;
 
@@ -17,10 +44,10 @@ void write_mandelmap(char *filename, unsigned char *mandelmap, size_t N, int cut
 	}
 
 	/* Put structural information */
-	fprintf(fp, "P5\n%ld %ld\n%d\n", N, N, cutoff);
+	fprintf(fp, "P5\n%ld %ld\n255\n", width, height);
 
 	/* Output grayscale pixels */
-	fwrite(mandelmap, sizeof(unsigned char), N * N, fp);
+	fwrite(mandelmap, sizeof(unsigned char), width * height, fp);
 
 	// free(pixels);
 	fclose(fp);
@@ -29,7 +56,7 @@ void write_mandelmap(char *filename, unsigned char *mandelmap, size_t N, int cut
 int main(int argc, char *argv[])
 {
 	float sigma, order;
-	char *input_filename, output_filename;
+	char *output_filename;
     int width, height;
     FILE *input_file;
 
@@ -54,11 +81,29 @@ int main(int argc, char *argv[])
 		exit(1);
     }
     order = ceil(6 * sigma);
+    if ((int)order % 2 == 0) {
+        order++;
+    }
     if (order > width || order > height) {
         fprintf(stderr, "Error: sigma value too big for image size");
 		exit(1);
     }
 
+    float *kernel = aligned_alloc(64, (int) order * (int) order * sizeof(float));
+    unsigned char *image_mat = aligned_alloc(64, width * height * sizeof(unsigned char));
+
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            fread(image_mat[i * width + j], sizeof(unsigned char), 1, input_file);
+        }
+    } 
+
+    for (int i = 0; i < order; i++) {
+        for (int j = 0; j < order; j++) {
+            kernel[i * (int) order + j] = (1/(2*M_PI*sigma*sigma)) * 
+            exp(-(pow(i - floor(order/2), 2) + pow(j - floor(order/2), 2))/(2 * sigma * sigma));
+        }
+    }
 
 	/* Align allocation on a cache line (64 bytes) */
 	// mandelmap = aligned_alloc(64, N * N * sizeof(char));
@@ -67,13 +112,8 @@ int main(int argc, char *argv[])
 	// mandel_calc(mandelmap, N, x_coord, y_coord, zoom_level, cutoff);
 
 	// /* Save output image */
-	// filename = malloc(PATH_MAX);
-	// sprintf(filename, "mandel_%d_%.3lf_%.3lf_%.3lf_%d",
-	// 		N, x_coord, y_coord, zoom_level, cutoff);
-	// if (argc > 6)
-	// 	strcat(filename, argv[6]);
-	// strcat(filename, ".pgm");
-	// write_mandelmap(filename, mandelmap, N, cutoff);
+	write_mandelmap(output_filename, image_mat, width, height);
+    fclose(input_file);
 	// free(filename);
 
 	return 0;
